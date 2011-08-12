7b91ed33306cb0a0cf575934b761e96ca8ac8ee3
Neil Benn <n.j.l.benn@gmail.com>
2011-08-12 15:49:32 +0100

    Reskin Cohere: [Help page]
    - Removed link to RDF documentation
    - Removed link to support page
    - Removed link to contact page
    - Added two sections: Screencasts and API
    - Added links to Cohere screencasts on YouTube
    - Amended Copyright notice

--- help/index.php
+++ help/index.php
@@ -1,6 +1,7 @@
 <?php
 /********************************************************************************
  *                                                                              *
+ *  (c) Copyright 2011 University of Leeds, UK                                  *
  *  (c) Copyright 2010 The Open University UK                                   *
  *                                                                              *
  *  This software is freely distributed in accordance with                      *
@@ -28,15 +29,60 @@
 
     <h1>Help</h1>
 
-    <p>For help getting started with Cohere, then please have a look at <a href="<?php print($CFG->homeAddress)?>index.php#screencast">our screencasts</a>.</p>
+    <h2 id="screencasts">Screencasts</h2>
+    <p>In the future, we will provide screencasts that explain how to
+			use PolicyCommons. In the meantime, following are some links to
+			YouTube videos about how to use Cohere, which is the software
+			powering PolicyCommons. Note, of course, that there will be some
+			differences in the user interface between the two tools.</p>
 
-    <p>For details of how to use our RESTful API, please visit the <a href="code-doc/Cohere-API/apilib.html">API documentation</a>. </p>
+			<h3>Introduction to Cohere</h3>
+			<div style="margin-bottom: 3em;">
+			  <object width="425" height="350">
+			    <param name="movie"
+			           value="http://www.youtube.com/v/eBS03VoWWAA"></param>
+			    <embed src="http://www.youtube.com/v/eBS03VoWWAA"
+                 type="application/x-shockwave-flash"
+                 width="425" height="350"></embed>
+			  </object>
+			</div>
 
-    <p>For help with using RDF with Cohere, please visit out <a href="rdf.php">RDF how-to</a>.</p>
+			<h3>Cohere -- How to add an Idea</h3>
+			<div style="margin-bottom: 3em;">
+			  <object width="425" height="350">
+			    <param name="movie"
+			           value="http://www.youtube.com/v/xx4qBVewrr4"></param>
+			    <embed src="http://www.youtube.com/v/xx4qBVewrr4"
+                 type="application/x-shockwave-flash"
+                 width="425" height="350"></embed>
+			  </object>
+			</div>
 
-    <p>If you would like to report a bug or have suggestions for improvements to Cohere, then please visit our <a href="<?php print($CFG->supportAddress)?>">support site</a>.</p>
+			<h3>Cohere -- How to add a Connection</h3>
+			<div style="margin-bottom: 3em;">
+			  <object width="425" height="350">
+			    <param name="movie"
+			           value="http://www.youtube.com/v/nxsUD-egDKQ"></param>
+			    <embed src="http://www.youtube.com/v/nxsUD-egDKQ"
+                 type="application/x-shockwave-flash"
+                 width="425" height="350"></embed>
+			  </object>
+			</div>
 
-    <p>If your query isn't answered by any of the above, then please <a href="mailto:<?php print($CFG->EMAIL_REPLY_TO)?>">contact us</a>.</p>
+			<h3>Cohere -- How to add a Website</h3>
+			<div style="margin-bottom: 3em;">
+			  <object width="425" height="350">
+			    <param name="movie"
+			           value="http://www.youtube.com/v/0Dv_EnFY_8g"></param>
+			    <embed src="http://www.youtube.com/v/0Dv_EnFY_8g"
+                 type="application/x-shockwave-flash"
+                 width="425" height="350"></embed>
+			  </object>
+			</div>
+
+		<h2 id="api">API</h2>
+			<p>The underlying PolicyCommons (Cohere) API is described on the
+			<a href="code-doc/Cohere-API/apilib.html">API documentation</a> page.</p>
 
 <?php
     include_once("../includes/footer.php");
