cf10683d620cbb499a6b7ae13ca39a062415c15e
Neil Benn <n.j.l.benn@gmail.com>
2011-12-15 14:23:59 +0000

    Display Debate as Treemap
    - Add new file "visualize/debate-treemap.js" which loads the contents of a debate and
    visualises using the Treemap layout in the D3 JavaScript library.
    - Modify 'includes/tabber.js':
      = Remove old code for visualising debate contents as a list of nodes
      = Dynamically load the JavaScript in "visualize/debate-treemap.js"

---
+++ visualize/debate-treemap.js
@@ -0,0 +1,140 @@
+/********************************************************************************
+ *                                                                              *
+ *  (c) Copyright 2011 University of Leeds, UK                                  *
+ *  (c) Copyright 2010 The Open University UK                                   *
+ *                                                                              *
+ *  This software is freely distributed in accordance with                      *
+ *  the GNU Lesser General Public (LGPL) license, version 3 or later            *
+ *  as published by the Free Software Foundation.                               *
+ *  For details see LGPL: http://www.fsf.org/licensing/licenses/lgpl.html       *
+ *               and GPL: http://www.fsf.org/licensing/licenses/gpl-3.0.html    *
+ *                                                                              *
+ *  This software is provided by the copyright holders and contributors "as is" *
+ *  and any express or implied warranties, including, but not limited to, the   *
+ *  implied warranties of merchantability and fitness for a particular purpose  *
+ *  are disclaimed. In no event shall the copyright owner or contributors be    *
+ *  liable for any direct, indirect, incidental, special, exemplary, or         *
+ *  consequential damages (including, but not limited to, procurement of        *
+ *  substitute goods or services; loss of use, data, or profits; or business    *
+ *  interruption) however caused and on any theory of liability, whether in     *
+ *  contract, strict liability, or tort (including negligence or otherwise)     *
+ *  arising in any way out of the use of this software, even if advised of the  *
+ *  possibility of such damage.                                                 *
+ *                                                                              *
+ ********************************************************************************/
+
+function loadMap(){
+
+	var tb1 = new Element("div", {'id':'netbuttons', 'class':'toolbarrow'});
+	$("tab-content-debatemap").update(tb1);
+	tb1.insert(displayConnectionAdd());
+	tb1.insert(displayConnectionVisualisations('net'));
+	tb1.insert(displaySnippetButtons(SNIPPET_CONNECTION_NET));
+
+	var tb2 = new Element("div", {'id':'connmessagediv','class':'toolbarrow'});	
+	var messagearea = new Element("div", {'id':'connmessage','class':'toolbitem'});	
+	tb2.insert(messagearea);
+
+	$("tab-content-debatemap").insert(tb2);
+
+		// Load the Connection Net data
+    var loadDiv = new Element("div",{'class':'loading'});
+    loadDiv.insert("<img src='"+URL_ROOT+"images/ajax-loader.gif'/>");
+    loadDiv.insert("<br/>(Loading Debate Map...)");
+
+		$('connmessage').update(loadDiv);
+	
+		var args = Object.clone(NET_ARGS);
+		args["start"] = 0;
+
+		//get all (not just the normal 20 max)
+		args["max"] = -1;
+	
+		//request to get the current connections  
+	  var reqUrl = SERVICE_ROOT + "&method=getdebatecontents&" + Object.toQueryString(args);
+
+		d3.json(reqUrl, function(cohereJson) {
+				var d3Json = convertCohereNodesetJsonToD3(cohereJson);
+
+				//set the count in tab header
+      	$('map-elements-count').innerHTML = "";
+      	$('map-elements-count').insert(cohereJson.nodeset[0].totalno);
+
+				drawDebateMap(d3Json);
+		});
+
+//		drawDebateMap();
+    $('connmessage').innerHTML="";	
+}
+
+function convertCohereNodesetJsonToD3 (cohereJson) {
+		var d3Json = {
+				children: []
+		};
+
+		var nodes = cohereJson.nodeset[0].nodes;
+
+		for (var i=0, len=nodes.length; i<len; i++) {
+				var newNode = Object.clone(nodes[i].cnode);
+				d3Json.children.push(newNode);
+		}
+
+		return d3Json;
+}
+
+function drawDebateMap(data) {
+	
+		// Set width & height for SVG
+		var debatemapDiv = new Element("div", {"id":"debatemap-div"});
+		$("tab-content-debatemap").insert(debatemapDiv);
+
+		var w = $('tab-content-debatemap').offsetWidth - 30;
+		var h = getWindowHeight();
+		var color = d3.scale.category20c();
+
+		var vis = d3.select("#debatemap-div")
+				.append("div")
+				.style("position", "relative")
+				.style("width", w + "px")
+				.style("height", h + "px")
+				.style("top", "40px");
+
+		vis.style("opacity", 1e-6)
+				.transition()
+				.duration(1000)
+				.style("opacity", 1);
+
+var treemap = d3.layout.treemap()
+    .size([w, h])
+    .sticky(true)
+				.value(function(d) { return parseInt(d.connectedness); });
+
+  vis.data([data]).selectAll("div")
+      .data(treemap.nodes)
+    .enter().append("div")
+      .call(cell);
+
+function cell() {
+  this
+      .style("left", function(d) { return d.x + "px"; })
+      .style("top", function(d) { return d.y + "px"; })
+      .style("width", function(d) { return d.dx - 1 + "px"; })
+      .style("height", function(d) { return d.dy - 1 + "px"; })
+				.style("background", function(d) { return d.children ? color(d.name) : color(d.name); })
+				.html(function(d) {
+						return d.children ? null : ((d.role[0].role.name == "Debate") ?
+								 "<a href='"+createDebateURL(d.nodeid)+"'>"+d.name+"</a>" :
+								 "<a href='"+createNodeURL(d.nodeid)+"'>"+d.name+"</a>"); })
+
+		//Move the following to CSS -- create class called "treemap-cell"
+				.style("border", "solid 1px white")
+				.style("font", "12px sans-serif")
+				.style("line-height", "12px")
+				.style("overflow", "hidden")
+				.style("position", "absolute")
+				.style("text-indent", "2px");
+      //.attr("class", "treemap-cell");
+}
+}
+
+loadMap();
