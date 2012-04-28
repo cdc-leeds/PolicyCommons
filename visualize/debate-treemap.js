5d29765b9200aaa88a3900bc6c6f0d678f8fffca
Neil Benn <n.j.l.benn@gmail.com>
2012-04-28 21:51:02 +0100

    Code-cleanup: visualize/debate-treemap.js: Fixed indentation and other issues from JSHint

--- visualize/debate-treemap.js
+++ visualize/debate-treemap.js
@@ -34,7 +34,9 @@ function loadMap() {
     args.max = -1;
 
     //request to get the current connections
-    var reqUrl = SERVICE_ROOT + "&method=getdebatecontents&" + Object.toQueryString(args);
+    var reqUrl = SERVICE_ROOT +
+        "&method=getdebatecontents&" +
+        Object.toQueryString(args);
 
     d3.json(reqUrl, function (cohereJson) {
         var d3Json = convertCohereNodesetJsonToD3(cohereJson);
@@ -84,86 +86,86 @@ function drawDebateMap(data) {
         .duration(1000)
         .style("opacity", 1);
 
-var treemap = d3.layout.treemap()
-    .size([w, h])
-    .sticky(true)
-    .value(function(d) {
-        // Make size of region in debate map be determined based on
-        // Log of the number of responses. Use log so that variation
-        // in size isn't too much. Need to add 2 to num_responses so
-        // that if number of responses is 0 we still get the cell to
-        // display (adding 1 would give log(1) which is 0). There
-        // probably is a more elegant way of doing this.
-        return Math.log(parseInt(d.num_responses)+2); })
+    var treemap = d3.layout.treemap()
+        .size([w, h])
+        .sticky(true)
+        .value(function (d) {
+            // Make size of region in debate map be determined based on
+            // Log of the number of responses. Use log so that variation
+            // in size isn't too much. Need to add 2 to num_responses so
+            // that if number of responses is 0 we still get the cell to
+            // display (adding 1 would give log(1) which is 0). There
+            // probably is a more elegant way of doing this.
+            return Math.log(parseInt(d.num_responses, 10) + 2); })
 
     // Sort so largest cell in treemap is at the top-left rather than
     // bottom right
-        .sort(function(a, b) { return a.value - b.value});
+        .sort(function (a, b) { return a.value - b.value; });
 
-  vis.data([data]).selectAll("div")
-      .data(treemap.nodes)
-    .enter().append("div")
-      .call(cell);
+    vis.data([data]).selectAll("div")
+        .data(treemap.nodes)
+        .enter().append("div")
+        .call(cell);
 
-function cell() {
-  this
-      .style("left", function(d) { return d.x + "px"; })
-      .style("top", function(d) { return d.y + "px"; })
-      .style("width", function(d) { return d.dx - 1 + "px"; })
-      .style("height", function(d) { return d.dy - 1 + "px"; })
-        .style("background", function(d) {
-            var args = Object.clone(NET_ARGS);
-            if (d.nodeid != undefined)
-                // Sub-Debates (i.e. sections within a Debate) should
-                // have a unique background colour in the treemap. But
-                // Issues in a treemap should have the same background
-                // colour as the Sub-Debate or section they are
-                // contained in.
-                return (d.role[0].role.name == "Issue") ?
-                         color(args.nodeid) : color(d.nodeid); })
-        .html(function(d) {
-            // Make text in each treemap cell be a hyperlink. If cell
-            // is a Debate then make hyperlink to Debate URL, else
-            // then assume cell is an Issue and make hyperlink to
-            // Issue URL.
-            return d.children ? null : cell_html(d); })
+    function cell() {
+        this
+            .style("left", function (d) { return d.x + "px"; })
+            .style("top", function (d) { return d.y + "px"; })
+            .style("width", function (d) { return d.dx - 1 + "px"; })
+            .style("height", function (d) { return d.dy - 1 + "px"; })
+            .style("background", function (d) {
+                var args = Object.clone(NET_ARGS);
+                if (d.nodeid !== undefined) {
+                    // Sub-Debates (i.e. sections within a Debate) should
+                    // have a unique background colour in the treemap. But
+                    // Issues in a treemap should have the same background
+                    // colour as the Sub-Debate or section they are
+                    // contained in.
+                    return (d.role[0].role.name === "Issue") ?
+                        color(args.nodeid) : color(d.nodeid); }})
+            .html(function(d) {
+                // Make text in each treemap cell be a hyperlink. If cell
+                // is a Debate then make hyperlink to Debate URL, else
+                // then assume cell is an Issue and make hyperlink to
+                // Issue URL.
+                return d.children ? null : cell_html(d); })
 
-        .attr("class", "debatemap-cell");
+            .attr("class", "debatemap-cell");
 
-    function cell_html (d) {
-        var html = d.name;
-        var cell_type = d.role[0].role.name;
+        function cell_html (d) {
+            var html = d.name;
+            var cell_type = d.role[0].role.name;
 
-        if (cell_type === "Debate") {
-            html = html + "<br /><br />" +
-                "(Issues: " + d.num_issues + ")" + "<br />" +
-                "(Responses: " + d.num_responses + ")";
+            if (cell_type === "Debate") {
+                html = html + "<br /><br />" +
+                    "(Issues: " + d.num_issues + ")" + "<br />" +
+                    "(Responses: " + d.num_responses + ")";
 
-            // Only if the number of issues is more than 0 do we add a
-            // hyperlink for Sub-Debate cells. (In principle there
-            // should always be issues in debates/sub-debates, but in
-            // practice the modeller might not always get around to
-            // modelling the issues within a debate/sub-debate.)
-            if (d.num_issues > 0) {
-                html =
-                    "<a href='"+createDebateURL(d.nodeid)+"'>" +
-                    html + "</a>";
+                // Only if the number of issues is more than 0 do we add a
+                // hyperlink for Sub-Debate cells. (In principle there
+                // should always be issues in debates/sub-debates, but in
+                // practice the modeller might not always get around to
+                // modelling the issues within a debate/sub-debate.)
+                if (d.num_issues > 0) {
+                    html =
+                        "<a href='"+createDebateURL(d.nodeid)+"'>" +
+                        html + "</a>";
+                }
+            } else if (cell_type === "Issue") {
+                html = html + "<br /><br />" +
+                    "(Responses: " + d.num_responses + ")";
+
+                // Only if the number of responses is more than 0 do we
+                // add a hyperlink to Issue cells.
+                if (d.num_responses > 0) {
+                    html =
+                        "<a href='"+createIssueURL(d.nodeid)+"'>" +
+                        html + "</a>";
+                }
             }
-        } else if (cell_type === "Issue") {
-            html = html + "<br /><br />" +
-                "(Responses: " + d.num_responses + ")";
-
-            // Only if the number of responses is more than 0 do we
-            // add a hyperlink to Issue cells.
-            if (d.num_responses > 0) {
-                html =
-                    "<a href='"+createIssueURL(d.nodeid)+"'>" +
-                    html + "</a>";
-            }
+            return html;
         }
-        return html;
     }
-}
 }
 
 loadMap();
