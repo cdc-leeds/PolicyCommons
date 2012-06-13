82ca0771da0246353e7344d8f01243de8108f8c3
Neil Benn <n.j.l.benn@gmail.com>
2012-06-13 10:49:14 +0100

    PhpLib/Formats: XXX: Bug fix for running PHP in Quercus
    - In Quercus, get_object_vars returns empty array if run on
      non-object, whereas in standard PHP implementations it returns
      NULL. So now have to force value to NULL.

--- phplib/formats/json.php
+++ phplib/formats/json.php
@@ -60,7 +60,11 @@ class format_json extends format_base {
                     $str .= '{';
                 }
                 for($j=0;$j< sizeof($keys); $j++){
-                    $attr2 = get_object_vars($attr[$keys[$j]]);
+
+                    $attr2 = (is_object($attr[$keys[$j]])) ?
+                      get_object_vars($attr[$keys[$j]]) :
+                      NULL;
+
                     if(is_array($attr2) || is_array($attr[$keys[$j]])){ 
                         $str .= $this->phpToJSONInner($keys[$j],$attr[$keys[$j]]);
                     } else {
@@ -133,8 +137,11 @@ class format_json extends format_base {
             $str .= '{';
             
             for($j=0;$j< sizeof($keys); $j++){
+
+                $attr2 = (is_object($attr[$keys[$j]])) ?
+                  get_object_vars($attr[$keys[$j]]) :
+                  NULL;
                 
-                $attr2 = get_object_vars($attr[$keys[$j]]);
                 if(is_array($attr2) || is_array($attr[$keys[$j]])){ 
                     $str .= $this->phpToJSONInner($keys[$j],$attr[$keys[$j]]);
                 } else {
